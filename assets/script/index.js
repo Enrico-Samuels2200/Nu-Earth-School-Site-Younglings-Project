342cff867a9d8dcdb3dab910a82da81124925bb8
Enrico-Samuels2200 <52998518+Enrico-Samuels2200@users.noreply.github.com>
2021-05-25 00:33:30 +0200

    Updated index.html and index.js
    In index.html updated function call from 'sendD' to 'sendToDb'.
    In index.js updated function name from 'sendD' to 'sendToDb', changed variable name from 'a' to 'userData', and added a POST request to an API.

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,9 +1,22 @@
-let a = {}
+let userData = {}
 
 updateForm = (name, data) => {
-    a[name] = data;
+    userData[name] = data;
 }
 
-sendD = () => {
+sendToDB = () => {
+    // POST request with body equal on data in JSON format
+    fetch('http://localhost:3535/api/send', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    })
+    .then((response) => response.json())
     
+    // If error occurs then it'll be recorded
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
